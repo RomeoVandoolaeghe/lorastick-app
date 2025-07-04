24b731ca1715f2f8dc1da938f9815261f4e0f5e6
Roméo Vandoolaeghe <romeo.vandoolaeghe@student.junia.com>
2025-07-04 15:35:38 -0400

    comments

--- stickboard/sticksoftmain/BLE-AT_bridge.ino
+++ stickboard/sticksoftmain/BLE-AT_bridge.ino
@@ -1,12 +1,16 @@
-void setup() {
-  Serial.begin(115200);                // Pour affichage debug sur port USB
-  Serial6.begin(115200, RAK_AT_MODE);  // BLE en mode AT
+//BLE-AT bridge
+// This sketch allows the LoraStick to communicate with a BLE device using AT commands.
+// It uses the RAKwireless AT commands to manage BLE connections and data transfer.
 
-  // Démarrer la publicité BLE pour permettre la connexion
-  api.ble.advertise.start(30);
+void setup() {
+  Serial.begin(115200);                // Serial monitor at 115200 baud
+  Serial6.begin(115200, RAK_AT_MODE);  // Serial6 for AT commands at 115200 baud
+
+  api.ble.advertise.start(30); // Start advertising for 30 seconds for BLE connections
   Serial.println("BLE-AT bridge started. Waiting for BLE connection...");
 }
 
-void loop() {
 
+void loop() {
+  
 }
